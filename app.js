7f81ba9d4f1e8fe1db06a7601a0165192e3f8d82
Aid Thompson <thepandariot@gmail.com>
2021-07-05 12:20:24 +0100

    radius size mapped to birth numbers

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ var xScale = d3
   .range([padding, width - padding]);
 // no. of births / population = births-per-capita [rather than no. of births]
 
+var colorScale = d3
+  .scaleLinear()
+  .domain(d3.extent(birthData2011, (d) => d.population / d.area))
+  .range(["lightgreen", "black"]);
+// mapping population density to colours!
+
+var radiusScale = d3
+  .scaleLinear()
+  .domain(d3.extent(birthData2011, (d) => d.births))
+  .range([2, 40]);
+// size of circle is mapped to no. of births
+
 d3.select("svg")
   .attr("height", height)
   .attr("width", width)
@@ -28,4 +40,5 @@ d3.select("svg")
   .append("circle")
   .attr("cx", (d) => xScale(d.births / d.population))
   .attr("cy", (d) => yScale(d.lifeExpectancy))
-  .attr("r", 5);
+  .attr("r", (d) => radiusScale(d.births))
+  .attr("fill", (d) => colorScale(d.population / d.area));
